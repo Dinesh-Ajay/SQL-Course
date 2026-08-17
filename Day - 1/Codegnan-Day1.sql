569977acfd9f3f6694f191c3d1fc1a7d360dfabc
Dinesh Ajay <dineshajay874@gmail.com>
2026-08-17 23:13:25 +0530

    Update Codegnan-Day1.sql

--- Day - 1/Codegnan-Day1.sql
+++ Day - 1/Codegnan-Day1.sql
@@ -3,7 +3,7 @@ show databases;            -- Display the DataBases
 use mysqldatabase;      -- Using current DataBases
 show tables;            -- Displaying Tables in the current database
 
--- Performing Commands
+-- Performing Mysql Commands
 -- 1) DDL (Data Defination Language) --> Create,alter,truncate,drop,rename
 -- 2) DML (Data Manipulation Language) -->insert,delete,update
 -- 3) DCL (Data control Language) --> Grant,revoke
